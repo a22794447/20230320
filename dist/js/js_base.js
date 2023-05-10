60e1ea21d9899ea82456445095ac1d547db43f76
SXuerou <a22794447@gmail.com>
2023-05-10 21:30:08 +0800

    js基礎語法

---
+++ dist/js/js_base.js
@@ -0,0 +1,8 @@
+let age = 18;
+let firstName = "Xuerou";
+let lastName = "lin";
+let country = "TW";
+
+console.log('(' + country + ') ' + firstName + ' ' + lastName);
+
+console.log(`(${country}) ${firstName} ${lastName}`)
