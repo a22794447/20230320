c97437f34fd815158d49e03c7a5051e7892e66df
SXuerou <a22794447@gmail.com>
2023-05-17 20:12:46 +0800

    99表格

--- dist/js/js_99.js
+++ dist/js/js_99.js
@@ -1,16 +1,16 @@
-let main = document.querySelector('#main');
-console.log(main);
-// main.innerHTML = 'Super man!';
+// let main = document.querySelector('#main');
+// console.log(main);
+// // main.innerHTML = 'Super man!';
 
-let lis = document.querySelectorAll('#menu li');
-console.log(lis);
+// let lis = document.querySelectorAll('#menu li');
+// console.log(lis);
 
-let lis2 = document.querySelector('#menu li');
-console.log(lis2);
+// let lis2 = document.querySelector('#menu li');
+// console.log(lis2);
 
-let num1 = document.querySelector('#num-1');
-let showBtn = document.querySelector('#show-btn');
+// let num1 = document.querySelector('#num-1');
+// let showBtn = document.querySelector('#show-btn');
 
-showBtn.addEventListener('click', () => {
-    num1.value = 'ABC';
-})
+// showBtn.addEventListener('click', () => {
+//     num1.value = 'ABC';
+// })
