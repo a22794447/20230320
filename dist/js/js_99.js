97c837f54bf54e3e32cb9f1088d1bd61fc33bc6e
SXuerou <a22794447@gmail.com>
2023-05-17 19:31:06 +0800

    抓取//控制

---
+++ dist/js/js_99.js
@@ -0,0 +1,9 @@
+let main = document.querySelector('#main')
+console.log(main)
+// main.innerHTML = 'Super man!'
+
+let lis = document.querySelectorAll('#menu li')
+console.log(lis)
+
+let lis2 = document.querySelector('#menu li')
+console.log(lis2)
