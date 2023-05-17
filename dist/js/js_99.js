93fc9ffd94d7bb05abd077d25eb4f347beb126ff
SXuerou <a22794447@gmail.com>
2023-05-17 20:45:31 +0800

    步驟1/2--抓取n1/n2

--- dist/js/js_99.js
+++ dist/js/js_99.js
@@ -16,7 +16,11 @@
 // })
 
 const genTable = () => {
-    console.log('123');
+    let n1 = document.querySelector('#n1');
+    let n2 = document.querySelector('#n2');
+
+    console.log(n1, n2);
+    console.log(n1.value, n2.value)
 }
 
 let mark = document.querySelector('#mark');
