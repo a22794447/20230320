981274ec90999ce2ba921ffbc16d9fdd73c73b09
SXuerou <a22794447@gmail.com>
2023-05-24 21:31:09 +0800

    使用 keyup 判斷 enter 觸發

--- dist/js/say_hi.js
+++ dist/js/say_hi.js
@@ -1,10 +1,10 @@
 const sayHi = () => {
-    let name = document.querySelector('#name')
+    // let name = document.querySelector('#name')
 
-    if (!name) {
-        alert('沒有輸入框');
-        return;
-    }
+    // if (!name) {
+    //     alert('沒有輸入框');
+    //     return;
+    // }
 
     if (!name.value) {
         alert('請輸入名字');
@@ -24,16 +24,27 @@ hi.addEventListener('click', sayHi);
 
 let name = document.querySelector('#name');
 
-name.addEventListener('change', () => {
-    // 觸發條件：輸入文字，離開輸入框，刪掉文字，離開輸入框
-    if (!name.value) {
-        alert('請輸入名字');
-        name.focus();
-    }
+// name.addEventListener('change', () => {
+//     // 觸發條件：輸入文字，離開輸入框，刪掉文字，離開輸入框
+//     if (!name.value) {
+//         alert('請輸入名字');
+//         name.focus();
+//     }
 
-    // console.log(name.value)
-})
+//     // console.log(name.value)
+// })
 
-name.addEventListener('blur', () => {
-    console.log(`blur: ${name.value}`);
-})
+// name.addEventListener('blur', () => {
+//     console.log(`blur: ${name.value}`);
+// })
+
+// name.addEventListener('keypress', () => {
+//         console.log(`keypress: ${name.value}`);
+//     })
+
+    name.addEventListener('keyup', (e) => {
+        if (e.key == 'Enter') {
+            sayHi();
+        }
+        // console.log(`keyup: ${name.value}`)
+    })
