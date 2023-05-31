977b8e25e03dd93e092f9a40d88ed86cb1ac17e0
SXuerou <a22794447@gmail.com>
2023-05-31 19:53:15 +0800

    按鈕與輸入框事件綁定

--- dist/js/todo.js
+++ dist/js/todo.js
@@ -1,1 +1,34 @@
 // alert('todo');
+
+const init = () => {
+    let addBtn = document.querySelector('#add-btn');
+    let itemName = document.querySelector('#item-name')
+
+    addBtn.addEventListener('click', () => {
+        let value = itemName.value;
+
+        if (!value) {
+            alert('請輸入項目名稱');
+            itemName.focus();
+            return;
+        }
+
+        console.log(itemName.value)
+    });
+
+    itemName.addEventListener('keyup', (e) => {
+        if(e.key == 'Enter') {
+            let value = itemName.value;
+
+        if (!value) {
+            alert('請輸入項目名稱');
+            itemName.focus();
+            return;
+        }
+
+        console.log(itemName.value)
+        }
+    })
+}
+
+init();
