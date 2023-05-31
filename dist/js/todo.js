8ca34617abef944efe4ee6314d2742c981cf1944
SXuerou <a22794447@gmail.com>
2023-05-31 20:20:42 +0800

    新增項目收納

--- dist/js/todo.js
+++ dist/js/todo.js
@@ -4,7 +4,9 @@ const init = () => {
     let addBtn = document.querySelector('#add-btn');
     let itemName = document.querySelector('#item-name')
 
-    addBtn.addEventListener('click', () => {
+    
+        // 新增項目收納
+    const doAddItem = () => {
         let value = itemName.value;
 
         if (!value) {
@@ -12,21 +14,16 @@ const init = () => {
             itemName.focus();
             return;
         }
-
-        console.log(itemName.value)
-    });
+    
+            console.log(value)   
+        }
+    addBtn.addEventListener('click', () => {
+        doAddItem();
+    })
 
     itemName.addEventListener('keyup', (e) => {
-        if(e.key == 'Enter') {
-            let value = itemName.value;
-
-        if (!value) {
-            alert('請輸入項目名稱');
-            itemName.focus();
-            return;
-        }
-
-        console.log(itemName.value)
+        if (e.key == 'Enter') {
+            doAddItem();
         }
     })
 }
