5c79829a342804312a97a67e52a7e4e1f4788e16
SXuerou <a22794447@gmail.com>
2023-06-21 20:45:23 +0800

    popup 切換 gsap 引用 事件鎖

--- dist/js/final_layout.js
+++ dist/js/final_layout.js
@@ -35,11 +35,35 @@ Vue.createApp({
 
 Vue.createApp({
     data() {
-        return{}
+        return{
+            isOpen: false,
+            isRun: false,
+        }
     },
     methods: {
         doToggle() {
-            console.log(123)
+            if(this.isRun) {
+                return;
+            }
+
+            this.isRun = true;
+            this.isOpen = !this.isOpen;
+
+            let wrap = this.$refs.wrap;
+            let popup = this.$refs.popup;
+
+            if(this.isOpen) {
+                gsap.to(popup, { opacity: 1, display: 'block', duration: 1});
+                gsap.to(wrap, { opacity: 0, display: 'none', duration: 0});
+            } else {
+                gsap.to(popup, { opacity: 0, display: 'none', duration: 0});
+                gsap.to(wrap, { opacity: 1, display: 'block', duration: 1});
+                
+            }
+
+            setTimeout(() => {
+                this.isRun = false;
+            }, 1000)
         }
     }
 }).mount('#patent-design')
